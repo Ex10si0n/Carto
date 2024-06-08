ef9ac964fd8d4bc6cffd0757b668ec7437f16f20
ex10si0n <yan.ex10si0n@icloud.com>
2024-06-08 16:43:33 -0400

    Initial Commit

---
+++ CartoUITests/CartoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CartoUITestsLaunchTests.swift
+//  CartoUITests
+//
+//  Created by Ex10si0n Yan on 6/8/24.
+//
+
+import XCTest
+
+final class CartoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
